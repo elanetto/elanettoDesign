a61b2b6ee8589ab08a5aad7a45835a86463cfcb9
Helflaa <146756162+Helflaa@users.noreply.github.com>
2025-03-19 16:48:15 +0100

    Feat: created CRUD operation for stickers
    -uninstalled typescript and only use js for backend
    -use sequelize to ensure tablestructure and vaildate data
    - created controller to handle functions
    -created models for data structure
    - created routes to handle routes

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import stickerRoutes from "./routes/stickerRoutes.js";
+import sequelize from "./config/db.js";
+
+// Sync the database (this updates the structure automatically)
+sequelize.sync({ alter: true })
+    .then(() => console.log("Database synced successfully"))
+    .catch((error) => console.error("Failed to sync database:", error));
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/stickers", stickerRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Elanetto's Sticker API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
