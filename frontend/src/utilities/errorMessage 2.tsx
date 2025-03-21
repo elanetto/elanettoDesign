b3695072ac3e9cde75c37d91e741611708eb1395
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-21 15:49:57 +0100

    deleted duplucated files

--- frontend/src/utilities/errorMessage 2.tsx
+++
@@ -1,7 +0,0 @@
-export function ErrorMessage({message}: { message: string }) {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h2 className="text-2xl font-bold text-red-500">Error: {message}</h2>
-        </div>
-    );
-}
--- frontend/src/utilities/loading 2.tsx
+++
@@ -1,7 +0,0 @@
-export function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h2 className="text-2xl font-bold text-primary">Loading...</h2>
-        </div>
-    );
-}
