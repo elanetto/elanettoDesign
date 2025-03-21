3843d328c4d4814acc179d16a667382a48db2805
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-21 15:18:50 +0100

    feat: implemented product management for admin
    - fetch and display products for admin page
    - created admin product card component
    - added function to add new product
    - add function to delete product
    - added constant file for BASE_URL for backend
    - added errorMsg and Loading component

--- frontend/src/routes/AddProductPage/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function AddProductPage() {
-    return (
-        <>This is Add new ProductPage</>
-    )
-}
---
+++ frontend/src/routes/AdminPage/AddProductPage/index.tsx
@@ -0,0 +1,10 @@
+import AddStickerForm from "../../../components/AddNewStickerForm";
+
+export default function AddProductPage() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Add New Sticker</h1>
+            <AddStickerForm />
+        </div>
+    );
+}
