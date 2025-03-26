9b7a64ab2415204507dd6c1a9bb8662a4d0d7081
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-03-26 21:02:34 +0100

    MobileHeader.tsx. Hover/clicked color changed from gray-600 to secondary-text color. Notification changed from red-600 to primary.

--- frontend/src/Layout/MobileHeader.tsx
+++ frontend/src/Layout/MobileHeader.tsx
@@ -1,32 +1,31 @@
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type MobileHeaderProps = {
-    cartItemCount: number;
+  cartItemCount: number;
 };
 
 export default function MobileHeader({ cartItemCount }: MobileHeaderProps) {
-    return (
-        <header className="flex justify-between items-center p-4 bg-white shadow-md">
-            <h1 className="text-lg font-semibold text-primary">ElanettoDesign</h1>
+  return (
+    <header className="flex justify-between items-center p-4 bg-white shadow-md">
+      <h1 className="text-lg font-semibold text-primary">ElanettoDesign</h1>
 
-            <div className="flex items-center gap-4">
-                <button className="text-black hover:text-gray-600">
-                    <FaSearch size={18} />
-                </button>
+      <div className="flex items-center gap-4">
+        <button className="text-black hover:text-secondary-text">
+          <FaSearch size={18} />
+        </button>
 
-                <div className="relative">
-                    <Link className="text-black hover:text-gray-600"
-                    to="/cart">
-                        <FaShoppingCart size={20} />
-                    </Link>
-                    {cartItemCount > 0 && (
-                        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-                            {cartItemCount}
-                        </span>
-                    )}
-                </div>
-            </div>
-        </header>
-    );
+        <div className="relative">
+          <Link className="text-black hover:text-secondary-text" to="/cart">
+            <FaShoppingCart size={20} />
+          </Link>
+          {cartItemCount > 0 && (
+            <span className="absolute -top-2 -right-2 bg-primary text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              {cartItemCount}
+            </span>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
